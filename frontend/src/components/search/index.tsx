2f38af41d751032fe6c352745552e2d7beab16d5
Tomáš Vladík <tom.vladik@gmail.com>
2025-02-16 23:25:59 +0100

    adjusting styles of controls

--- frontend/src/components/search/index.tsx
+++
@@ -1,5 +0,0 @@
-import styles from "./index.module.less";
-
-export const Search = ({ onChange }: { onChange: React.ChangeEventHandler }) => {
-    return <input id="search-box" className={styles.search} type="text" onChange={onChange} placeholder="Hledat text ..." />;
-};
